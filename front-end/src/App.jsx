6534d6529cf37f2b49822fb0d2e385a758585347
Niaz Baharudeen <niazbaharudeen@gmail.com>
2025-12-11 00:06:02 -0500

    Added a browser router to main.jsx for routing between pages. Added the navbar and routes to the Home and Favorites components in the App component.

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,13 +1,22 @@
 import "./App.css";
+import NavBar from "./components/NavBar";
+import Favorites from "./pages/Favorites";
 import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   //only one parent/root element can be returned by a component
   //fragments are like a placeholder for a parent element, empty tag as wrapper
   return (
-    <>
-      <Home></Home>
-    </>
+    <div>
+      <NavBar />
+      <main className="content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
