1096c8a7cd4550e377fba17dd5275789546130be
Niaz Baharudeen <niazbaharudeen@gmail.com>
2025-12-11 00:03:55 -0500

    Added state to the home page for persisting search queries and rerendering the page when a search is submitted.

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -1,6 +1,13 @@
 import MovieCard from "../components/MovieCard";
+import { useState } from "react";
 
 function Home() {
+  //defines state and the function to update state
+  //when state is changed, the component rerenders
+  //setSearchQuery called onChange
+  //state persists across rerenders until a page refresh
+  const [searchQuery, setSearchQuery] = useState("");
+
   //dummy array of movies
   const movies = [
     { id: 1, title: "John Wick", release_date: 2020 },
@@ -8,7 +15,10 @@ function Home() {
     { id: 3, title: "The Matrix", release_date: 1999 },
   ];
 
-  const handleSearch = () => {};
+  const handleSearch = (e) => {
+    e.preventDefault();
+    alert(searchQuery);
+  };
   //use map function to map each movie in the array using an anonymous function
   //each movie is mapped to a movie card, the movie is passed as a prop
   //for map to work, the key property is needed so React knows which component to update
@@ -20,6 +30,8 @@ function Home() {
           type="text"
           placeholder="Search for movies ..."
           className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
         ></input>
         <button type="submit" className="search-btn">
           Search
