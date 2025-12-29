4795e67ca12983daf620d6431abc0153bf03e951
Niaz Baharudeen <niazbaharudeen@gmail.com>
2025-12-28 19:44:14 -0500

    Fixed MovieService implementation by converting it to an interface and creating an impl file. Fixed error with API call, made it so it accepts the correct environment variable that sets an authorization bearer token header.

---
+++ back-end/src/main/java/com/niazbaharudeen/back_end/services/impl/MovieServiceImpl.java
@@ -0,0 +1,45 @@
+package com.niazbaharudeen.back_end.services.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+import com.niazbaharudeen.back_end.dtos.APIResponseDTO;
+import com.niazbaharudeen.back_end.dtos.MovieResponseDTO;
+import com.niazbaharudeen.back_end.services.MovieService;
+
+@Service
+public class MovieServiceImpl implements MovieService {
+
+    @Autowired
+    private Environment environment;
+
+    private RestClient createRestClient() {
+        return RestClient.builder()
+                .baseUrl("https://api.themoviedb.org/3")
+                .defaultHeaders(
+                        httpHeaders -> {
+                            httpHeaders.set("accept", "application/json");
+                            httpHeaders.set("Authorization", "Bearer " + environment.getProperty("env.api_token"));
+                        })
+                .build();
+    }
+
+    @Override
+    public List<MovieResponseDTO> getPopularMovies() {
+        // Call the external API and provide our API key
+        // Using Spring's native mapping and multiple data objects, get the list of
+        // movies from the JSON response
+        RestClient restClient = createRestClient();
+        APIResponseDTO apiResponseDTO = restClient
+                .get()
+                .uri("/movie/popular")
+                .retrieve()
+                .body(APIResponseDTO.class);
+        return apiResponseDTO.getMovieResponseDTOs();
+    }
+
+}
